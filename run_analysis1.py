03d0e64653a99085ac5f391acd0ad5839ddfaae4
A. Flaxman <abie@uw.edu>
2011-05-12 14:19:10 -0700

    a small change to use adaptive metropolis to update U

--- run_analysis1.py
+++ run_analysis1.py
@@ -4,6 +4,7 @@ from pylab import hist, show
 from pymc import Matplot
 
 M = pymc.MCMC(analysis1)
+M.use_step_method(pymc.AdaptiveMetropolis, M.U)
 M.sample(iter=100000, burn=10000, thin=10, verbose=2)
 
 fit = M.stats()
