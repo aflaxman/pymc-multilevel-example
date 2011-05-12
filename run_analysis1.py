49dea954c0c681ad74a2b427a2e6ca594e501c4f
A. Flaxman <abie@uw.edu>
2011-05-12 14:43:16 -0700

    AM all together (fast, but don't expect good results)

--- run_analysis1.py
+++ run_analysis1.py
@@ -5,7 +5,7 @@ from pylab import hist, show
 from pymc import Matplot
 
 M = pymc.MCMC(analysis1)
-M.use_step_method(pymc.AdaptiveMetropolis, [M.B, M.U])
+M.use_step_method(pymc.AdaptiveMetropolis, [M.B, M.U, M.var_e1, M.var_u])
 M.sample(iter=100000, burn=50000, thin=10, verbose=2)
 
 fit = M.stats()
