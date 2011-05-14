e8545ca5bec6e3a1e5dbb7ba20bb957bc67c0d95
A. Flaxman <abie@uw.edu>
2011-05-14 16:54:01 -0700

    Make summary plots include autocorrelation of trace
    Cause, how else are you going to know if the chain has converged?
    p.s. This could become my first contribution back to the pymc codebase :)

--- run_analysis1.py
+++ run_analysis1.py
@@ -2,7 +2,9 @@ import pymc
 import analysis1
 reload(analysis1)
 from pylab import hist, show
-from pymc import Matplot
+#from pymc import Matplot
+import Matplot
+reload(Matplot)
 
 # Generate reasonable initial values by maximizing posterior with
 # variances held fixed
@@ -12,7 +14,7 @@ M.fit(method='fmin_powell', verbose=1)
 # Sample from full posterior distribution
 M = pymc.MCMC(analysis1)
 M.use_step_method(pymc.AdaptiveMetropolis, [M.B, M.U, M.var_e1, M.var_u])
-M.sample(iter=100000, burn=50000, thin=10, verbose=2)
+M.sample(iter=20000, burn=10000, thin=10, verbose=2)
 
 fit = M.stats()
 for k in fit.keys():
