11ff14dab2377491e8d4d155acfd74b7ca6c93d0
A. Flaxman <abie@uw.edu>
2011-05-15 16:29:29 -0700

    step 2 of alternative data-wrangling code: fully simplified

--- analysis1.py
+++ analysis1.py
@@ -3,37 +3,15 @@ import numpy as np
 import pymc
 
 run1 = pl.csv2rec('run1.csv')
-pn_data = np.genfromtxt('run1.csv', delimiter=",", unpack=True, skip_header=1)
-
-pid = pn_data[0] 	#subject id
-assert pl.all(run1.i == pid)
-
-gid = pn_data[1].astype(int) 	#group (cluster) id
-assert pl.all(run1.j == gid)
-
-tx = pn_data[2].reshape(200,1) 	#treatment condition (1 = clustered, 0 = unclustered)
-assert pl.all(run1.treat == tx.T)
-
-y = pn_data[3] 	#outcome variable
-assert pl.all(run1.y == y)
 
 ##Design matrix for intercept and intervention effect
-inter = np.ones((len(pid),1), dtype=int)
-X = np.hstack((inter,tx))
-
-X2 = [[1., t_ij] for t_ij in run1.treat]
-assert pl.all(X == X2)
+X = [[1., t_ij] for t_ij in run1.treat]
 
 ##Design matrix for cluster effect
-Z = (gid[:,None] == np.unique(gid)).astype(int)
-##Getting rid of the columns for the unclustered "clusters of 1"
-Z = Z[0:240:1,0:10:1]
-
-Z2 = pl.zeros([len(run1), 10])
+Z = pl.zeros([len(run1), 10])
 for row, j in enumerate(run1.j):
     if j <= 10:
-        Z2[row, j-1] = 1.
-assert pl.all(Z == Z2)
+        Z[row, j-1] = 1.
 
 ## Priors
 var_u = pymc.Gamma('var_u', alpha=1, beta=1, value=1.)
@@ -51,7 +29,7 @@ def y_hat(B=B, X=X, Z=Z, U=U):
     return np.dot(X,B)+np.dot(Z,U)
 
 @pymc.stochastic(observed=True)
-def y_i(value=y, mu=y_hat, tau=tau_e1):
+def y_i(value=run1.y, mu=y_hat, tau=tau_e1):
     return pymc.normal_like(value,mu,tau)
 
 
