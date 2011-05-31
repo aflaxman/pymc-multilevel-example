fd31d845b355a8afff30df68b7ebdac1fb20db2e
Thomas Wiecki <thomas.wiecki@gmail.com>
2011-05-31 12:38:44 -0400

    Tweaked model formulation a bit. Removed a couple of unneccessary lambdas, replaced decorated detereministic and stochastic with PyMC equivalents.

--- model.py
+++ model.py
@@ -14,19 +14,13 @@ for row, j in enumerate(data.j):
 
 ## Priors
 var_u = mc.Gamma('var_u', alpha=1, beta=1, value=1.)
-tau_u = mc.Lambda('tau_u', lambda v=var_u: v**-1, trace=False)
 
 B = mc.Normal('B', mu=[0, 0], tau=10000**-1)
 
-U = mc.Normal('u', mu=0, tau=tau_u, value=pl.zeros(10))
+U = mc.Normal('u', mu=0, tau=var_u**-1, value=pl.zeros(10))
 
 var_e1 = mc.Uniform('var_e1', lower=0, upper=100, value=[1., 1.])
-tau_e1 = mc.Lambda('tau_e1', lambda v=var_e1: v**-1, trace=False)
 
-@mc.deterministic(trace=False)
-def y_hat(B=B, X=X, Z=Z, U=U):
-    return pl.dot(X,B) + pl.dot(Z,U)
+y_hat = mc.LinearCombination('X*B', [X], [B]) + mc.LinearCombination('Z*U', [Z], [U])
 
-@mc.stochastic(observed=True)
-def y_i(value=data.y, mu=y_hat, tau=tau_e1):
-    return mc.normal_like(value,mu,tau[data.treat])
+y_i = mc.Normal('y_i', value=data.y, mu=y_hat, tau=var_e1[data.treat]**-1, observed=True)
